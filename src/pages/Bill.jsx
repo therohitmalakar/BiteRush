e4df497422de628bb7682706e1cb32cf015971a4
therohitmalakar <rohitmalakar437@gmail.com>
2025-09-02 17:04:06 +0530

    "running code"

---
+++ src/pages/Bill.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bill() {
+  return (
+    <div className='h-screen' >
+        <div className='justify-center items-center p-2 ' ></div>
+    </div>
+  )
+}
+
+export default Bill
